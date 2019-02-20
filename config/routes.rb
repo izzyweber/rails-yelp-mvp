9c72b48a9b4d580ee6193ceb8162a3e1751e8123
Izzy Weber <izzyweber@gmail.com>
2019-02-20 15:23:44 +0000

    create models, controllers, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/edit'
+  get 'restaurants/new'
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
